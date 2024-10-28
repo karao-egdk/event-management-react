0e6fe8bb74e29f99caa109f45df6b7a01304b7b8
Karthik J Rao <karao@eg.dk>
2024-10-28 10:13:04 +0530

    refactor: Get rid of unnecessary component

--- src/components/Search.tsx
+++
@@ -1,8 +0,0 @@
-
-function Search() {
-  return (
-    <input type='search' placeholder='Search for event' className='flex h-10 w-1/2 rounded-md border border-black/25 bg-background px-3 py-2 text-sm focus:border-0'/>
-  )
-}
-
-export default Search
--- src/pages/EventManagement.tsx
+++ src/pages/EventManagement.tsx
@@ -1,25 +1,25 @@
 import EventCard from "../components/EventCard";
-import Search from "../components/Search";
 import { Button } from "../components/ui/button";
 
 function EventManagement() {
-
     const eventDetails = {
         eventId: "",
-        title: 'Tech Conference 2023',
-        date: '2023-09-15',
-        location: 'San Francisco, CA',
+        title: "Tech Conference 2023",
+        date: "2023-09-15",
+        location: "San Francisco, CA",
         isEventDone: false,
-    }
+    };
 
     return (
         <section className="container mx-auto flex flex-col gap-5 py-10 px-2">
             <h1 className="text-3xl font-bold">Event Management</h1>
             <div className="flex justify-between items-center w-full">
-                <Search />
-                <Button>
-                    Create New Event
-                </Button>
+                <input
+                    type="search"
+                    placeholder="Search for event"
+                    className="flex h-10 w-1/2 rounded-md border border-black/25 bg-background px-3 py-2 text-sm focus:border-0"
+                />
+                <Button>Create New Event</Button>
             </div>
 
             <div className="grid gap-10 md:grid-cols-2 mx-auto">
