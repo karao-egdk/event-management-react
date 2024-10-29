d224a995f64c01504fd11d6790d91df3fa451c2f
Karthik J Rao <karao@eg.dk>
2024-10-29 16:59:15 +0530

    feat: store and retrieve data from local storage

--- src/context/reducer/eventReducer.ts
+++ src/context/reducer/eventReducer.ts
@@ -4,59 +4,23 @@ import {
     ReducerProps,
 } from "../../lib/interface";
 
+const addDataToLocalStorage = (allEvents: EventDetailsProp[]) => {
+    localStorage.setItem("events", JSON.stringify(allEvents));
+};
+
+const getDateFromLocalStorage = (): EventDetailsProp[] | null => {
+    const value = localStorage.getItem("events");
+
+    if (typeof value === "string") return JSON.parse(value);
+    return null;
+};
+
 export const initialState: EventContextInterface = {
-    events: [
-        {
-            eventId: "abc",
-            title: "Tech Conference 2023",
-            date: "2023-09-15",
-            location: "San Francisco, CA",
-            isEventDone: true,
-            budget: {
-                income: [
-                    {
-                        budgetId: "1",
-                        description: "Ticket Sales",
-                        amount: 5000,
-                    },
-                ],
-                expenses: [
-                    {
-                        budgetId: "1",
-                        description: "Venue Rental",
-                        amount: 2000,
-                    },
-                    { budgetId: "2", description: "Catering", amount: 1000 },
-                ],
-            },
-        },
-        {
-            eventId: "def",
-            title: "Startup Networking Mixer",
-            date: "2023-10-01",
-            location: "New York, NY",
-            isEventDone: true,
-            budget: {
-                expenses: [],
-                income: [],
-            },
-        },
-        {
-            eventId: "444",
-            title: "Tech Conference 2023",
-            date: "2024-10-31",
-            location: "Grande hotel Mangalore",
-            isEventDone: false,
-            budget: {
-                expenses: [],
-                income: [],
-            },
-        },
-    ],
+    events: getDateFromLocalStorage() || [],
 };
 
 const updateEventStatus = (events: EventDetailsProp[]) => {
-    console.log(events)
+    console.log(events);
     const updatedEvents = events.map((event) => {
         if (new Date(event.date) < new Date()) {
             const updatedEvent = {
@@ -64,8 +28,7 @@ const updateEventStatus = (events: EventDetailsProp[]) => {
                 isEventDone: true,
             };
             return updatedEvent;
-        }
-        else {
+        } else {
             const updatedEvent = {
                 ...event,
                 isEventDone: false,
@@ -85,35 +48,17 @@ const eventReducer = (
 
     switch (type) {
         case "ADD_EVENT":
-            return {
-                ...state,
-                events: updateEventStatus(payload.events),
-            };
         case "EDIT_EVENT":
-            return {
-                ...state,
-                events: updateEventStatus(payload.events),
-            };
         case "ADD_EXPENSE":
-            return {
-                ...state,
-                events: updateEventStatus(payload.events),
-            };
         case "ADD_INCOME":
-            return {
-                ...state,
-                events: updateEventStatus(payload.events),
-            };
         case "DELETE_EXPENSE":
-            return {
-                ...state,
-                events: updateEventStatus(payload.events),
-            };
         case "DELETE_INCOME":
-            return {
+            const updatedAllEvents = {
                 ...state,
                 events: updateEventStatus(payload.events),
             };
+            addDataToLocalStorage(updatedAllEvents.events);
+            return updatedAllEvents;
         default:
             return state;
     }
