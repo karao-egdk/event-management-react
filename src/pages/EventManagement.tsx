20629280efec3822ed0f2e59b9b41f97a6601eca
Karthik J Rao <karao@eg.dk>
2024-10-29 14:58:50 +0530

    feat: Implement search functionality

--- src/pages/EventManagement.tsx
+++ src/pages/EventManagement.tsx
@@ -1,10 +1,23 @@
+import React from "react";
 import EventCard from "../components/EventCard";
 import EventDialog from "../components/EventDialog";
 import useEvent from "../context/EventContext";
 
 function EventManagement() {
-
-    const eventDetails = useEvent().events;
+    const [search, setSearch] = React.useState<string>("");
+    const { events } = useEvent();
+    const eventDetails =
+        search === ""
+            ? events
+            : events.filter(
+                  (event) =>
+                      event.title
+                          .toLowerCase()
+                          .includes(search.toLowerCase()) ||
+                      event.location
+                          .toLowerCase()
+                          .includes(search.toLowerCase())
+              );
 
     return (
         <section className="container mx-auto flex flex-col gap-5 py-10 px-2">
@@ -13,6 +26,8 @@ function EventManagement() {
                 <input
                     type="search"
                     placeholder="Search for event"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
                     className="flex h-10 w-1/2 rounded-md border border-black/25 bg-background px-3 py-2 text-sm focus:border-0"
                 />
                 <EventDialog type="create" />
