bfe74acfbd4af7c874191e5d557860815aef3e30
Karthik <karao@eg.dk>
2024-11-12 10:52:27 +0530

    feat: create routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,39 @@
-import './App.css'
-import { EventProvider } from './context/EventContext'
-import EventManagement from './pages/EventManagement'
-
+import "./App.css";
+import { EventProvider } from "./context/EventContext";
+import EventManagement from "./pages/EventManagement";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Layout from "./components/Layout";
 
 function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "login",
+            element: <Login />,
+        },
+        {
+            path: "sign-up",
+            element: <Signup />,
+        },
+        {
+            path: "events",
+            element: <EventManagement />,
+        },
+    ]);
 
-  return (
-    <EventProvider>
-      <EventManagement />
-    </EventProvider>
-  )
+    return (
+        <EventProvider>
+            <Layout>
+                <RouterProvider router={router} />
+            </Layout>
+        </EventProvider>
+    );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import { Button } from "../components/ui/button";
+
+function Home() {
+    return (
+        <div className="max-w-screen-xl mx-auto px-4 py-28 gap-12 text-gray-600 md:px-8 xl:flex">
+            <div className="space-y-5 max-w-2xl mx-auto text-center xl:text-left">
+                <h1 className="text-4xl text-gray-800 font-extrabold mx-auto md:text-5xl">
+                    Create and view events
+                </h1>
+                <p className="max-w-xl mx-auto xl:mx-0">
+                    Sed ut perspiciatis unde omnis iste natus voluptatem
+                    accusantium doloremque laudantium, totam rem aperiam, eaque
+                    ipsa quae.
+                </p>
+                <div className="items-center justify-center gap-x-3 space-x-3 sm:space-x-0 space-y-3 sm:flex sm:space-y-0 xl:justify-start">
+                    <Button>View events</Button>
+                    <Button variant={'outline'}>Login Now</Button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
