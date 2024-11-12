f4c1b86a660286bf05e4b1b45a99e7340dd12261
Karthik <karao@eg.dk>
2024-11-12 15:55:35 +0530

    refactor: add variable to hold UUID

--- backend/management/src/main/java/events/entity/Event.java
+++ backend/management/src/main/java/events/entity/Event.java
@@ -1,5 +1,7 @@
 package events.entity;
 
+import java.util.UUID;
+
 import events.dto.BudgetDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,16 +11,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Event {
+	private UUID userId;
 	private String eventId;
 	private String title;
 	private String date;
 	private String location;
 	private BudgetDto budget;
 
-	public Event(String eventId, String title, String date, String location) {
+	public Event(String eventId, String title, String date, String location, UUID userId) {
 		this.eventId = eventId;
 		this.title = title;
 		this.date = date;
 		this.location = location;
+		this.userId = userId;
 	}
 }
